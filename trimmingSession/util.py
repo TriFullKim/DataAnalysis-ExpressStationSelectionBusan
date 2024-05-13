6a6413f8a175074d703c0d48a19d1d14a0f5984f
TriFullKim <mail@trifull.kim>
2024-05-14 03:47:16 +0900

    🧐 Data 추가 정제

--- trimmingSession/util.py
+++ trimmingSession/util.py
@@ -4,17 +4,23 @@ import pickle
 PATH_STATION_INFO = "../raw_data/selected/부산교통공사_도시철도 역명정보.csv"
 PATH_STATION_INFO_REFINE = "../trimmed_data/stationCodeNameLine.json"
 
-DAYTYPE = ['토요일', '일요일', '공휴일', '평일']
+DAYTYPE = ["토요일", "일요일", "공휴일", "평일"]
 PATH_STATION_SCHEDULE = "../raw_data/selected/부산교통공사_부산도시철도 운행 정보.csv"
-PATH_STATION_SCHEDULE_REFINE= "../trimmed_data/stationSchedule.json"
-PATH_STATION_SCHEDULE_REFINE_DAYTYPE = [f"../trimmed_data/stationSchedule{day}.json" for day in DAYTYPE]
-PATH_STATION_SCHEDULE_SEPERATED_DAYTYPE= [f"../trimmed_data/stationSchedule{day}-SEPERATED.json" for day in DAYTYPE]
+PATH_STATION_SCHEDULE_REFINE = "../trimmed_data/stationSchedule.json"
+PATH_STATION_SCHEDULE_REFINE_DAYTYPE = [
+    f"../trimmed_data/stationSchedule{day}.json" for day in DAYTYPE
+]
+PATH_STATION_SCHEDULE_SEPERATED_DAYTYPE = [
+    f"../trimmed_data/stationSchedule{day}-SEPERATED.json" for day in DAYTYPE
+]
 PATH_INTER_STATION_SPEND_TIME = "../trimmed_data/IntervalTimeAPI.pickle"
 PATH_INTER_STATION_EXCHANGE_TIME = "../trimmed_data/ExchangeTimeAPI.pickle"
 
 PATH_CUSTOMER_IO = "../raw_data/selected/부산교통공사_시간대별 승하차인원.csv"
 
-PATH_SCHEDULE_INTERVAL_MEAN=[f"../trimmed_data/ScheduleIntervalMean{day}.pickle" for day in DAYTYPE]
+PATH_SCHEDULE_INTERVAL_MEAN = [
+    f"../trimmed_data/ScheduleIntervalMean{day}.pickle" for day in DAYTYPE
+]
 PATH_WEIGHTED_MEAN_INTERVAL = "../trimmed_data/WeightedIntervalMean.pickle"
 # CONST
 DVECT_EIGEN = {
@@ -25,7 +31,7 @@ DVECT_EIGEN = {
     1: [
         "노포다대포해수욕장",
         "신평다대포해수욕장",
-        "노포신평", 
+        "노포신평",
     ],
     3: [
         "구명장산",
@@ -77,9 +83,11 @@ DVECT_EIGEN_REV = {
 }
 
 
-
 SUBWAY_CODE_PATH = PATH_STATION_INFO_REFINE
-def stationCode(_x, code_data:pd.DataFrame=pd.DataFrame(), return_mode="code"):
+
+
+def stationCode(_x, code_data: pd.DataFrame = pd.DataFrame(), return_mode="code"):
+
     result = None
     if pd.DataFrame().__len__() == 0:
         try:
@@ -98,44 +106,70 @@ def stationCode(_x, code_data:pd.DataFrame=pd.DataFrame(), return_mode="code"):
 
     return result
 
-def savePickle(_dat,path):
-    with open(path,mode="wb") as f:
-        pickle.dump(_dat,f)
+
+def savePickle(_dat, path):
+    with open(path, mode="wb") as f:
+        pickle.dump(_dat, f)
+
 
 def loadPickle(path):
-    with open(path, mode='rb') as f:
+    with open(path, mode="rb") as f:
         return pickle.load(f)
-    
+
+
 STATION_NameCode = loadPickle("../trimmed_data/stationNameCode.pickel")
 
-def df_mul_DiffColName(df1:pd.DataFrame,df2:pd.DataFrame,op_target:dict) -> pd.DataFrame:
-    _vals = list(op_target.values())
-    _rev_dict ={val:key for key,val in op_target.items()}
-    if list(op_target.keys())[0] in df1.columns:
-        _df1 = df1.rename(columns=op_target)
-        _df1.loc[:,_vals] = _df1.loc[:,_vals].mul(df2.loc[:,_vals])
-        return _df1.rename(columns=_rev_dict)
-    
-    elif list(op_target.keys())[0] in df2.columns:
-        _df2 = df2.rename(columns=op_target)
-        _df2.loc[:,_vals] = _df2.loc[:,_vals].mul(df1.loc[:,_vals])
-        return _df2.rename(columns=_rev_dict)
-    
+
+def df_mul_DiffColName(
+    df1: pd.DataFrame, df2: pd.DataFrame, operation_target: dict
+) -> pd.DataFrame:
+    values = list(operation_target.values())
+    reversed_dictionary = {val: key for key, val in operation_target.items()}
+    if list(operation_target.keys())[0] in df1.columns:
+        df_tmp = df1.rename(columns=operation_target)
+        df_tmp.loc[:, values] = df_tmp.loc[:, values].mul(df2.loc[:, values])
+        return df_tmp.rename(columns=reversed_dictionary)
+
+    elif list(operation_target.keys())[0] in df2.columns:
+        df_tmp = df2.rename(columns=operation_target)
+        df_tmp.loc[:, values] = df_tmp.loc[:, values].mul(df1.loc[:, values])
+        return df_tmp.rename(columns=reversed_dictionary)
+
     else:
         assert False, NotImplementedError
-        
-def df_div_DiffColName(df1:pd.DataFrame,df2:pd.DataFrame,op_target:dict) -> pd.DataFrame:
-    _vals = list(op_target.values())
-    _rev_dict ={val:key for key,val in op_target.items()}
-    if list(op_target.keys())[0] in df1.columns:
-        _df1 = df1.rename(columns=op_target)
-        _df1.loc[:,_vals] = _df1.loc[:,_vals].div(df2.loc[:,_vals])
-        return _df1.rename(columns=_rev_dict)
-    
-    elif list(op_target.keys())[0] in df2.columns:
-        _df2 = df2.rename(columns=op_target)
-        _df2.loc[:,_vals] = _df2.loc[:,_vals].div(df1.loc[:,_vals])
-        return _df2.rename(columns=_rev_dict)
-    
+
+
+def df_div_DiffColName(
+    df1: pd.DataFrame, df2: pd.DataFrame, operation_target: dict
+) -> pd.DataFrame:
+    values = list(operation_target.values())
+    reversed_dictionary = {val: key for key, val in operation_target.items()}
+    if list(operation_target.keys())[0] in df1.columns:
+        df_tmp = df1.rename(columns=operation_target)
+        df_tmp.loc[:, values] = df_tmp.loc[:, values].div(df2.loc[:, values])
+        return df_tmp.rename(columns=reversed_dictionary)
+
+    elif list(operation_target.keys())[0] in df2.columns:
+        df_tmp = df2.rename(columns=operation_target)
+        df_tmp.loc[:, values] = df_tmp.loc[:, values].div(df1.loc[:, values])
+        return df_tmp.rename(columns=reversed_dictionary)
+
     else:
         assert False, NotImplementedError
+
+# import datetime
+def convert_to_year_and_day(date_str):
+    if type(date_str) == int:
+        date_str = str(date_str)
+
+    try:
+        # Parse the input date string
+        date_obj = datetime.datetime.strptime(date_str, "%Y%m%d")
+
+        # Get the year and day of the year
+        year = date_obj.year
+        day_of_year = date_obj.timetuple().tm_yday
+
+        return year * 365 + day_of_year
+    except ValueError:
+        return "Invalid date format. Please provide a valid YYYYMMDD date."

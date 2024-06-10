feeed7dfd618256e3baf1bfe3fc57cb2c05b8e8e
seongmin0516 <gyu5242@naver.com>
2024-06-10 14:49:55 +0900

    Add explanation
    Add explanation about visualization

--- visualizationSession/implicit_metric_graph/implicit_metric_graph.py
+++ visualizationSession/implicit_metric_graph/implicit_metric_graph.py
@@ -4,33 +4,42 @@ import matplotlib.pyplot as plt
 # CSV 파일 로드
 s = pd.read_csv('sorted_implicit_metric1.csv', encoding='cp949')
 
+# 읽어온 데이터를 scode와 implicit_metric 컬럼만 있는 데이터프레임으로 만듦.
 df = pd.DataFrame(s, columns=["scode", "implicit_metric"])
+
+# scode 기준으로 데이터를 정렬하고, 인덱스를 리셋한 후, scode를 데이터프레임의 인덱스로 설정.
 implicit_metric = df.sort_values(by="scode").reset_index(drop=True)
 implicit_metric = implicit_metric.set_index("scode")
 
+# implicit_metric 컬럼의 최대값을 구하고, 그 값에 1.15를 곱하여 max 변수에 저장
 max = implicit_metric["implicit_metric"].max() * 1.15
 
+# 첫 번째 그래프의 크기를 설정, scode가 95부터 134까지의 데이터를 막대 그래프로 그림.
 plt.rcParams["figure.figsize"] = (39 * 3, 10)
 ax1 = implicit_metric.loc[95:134, :].plot.bar()
 fig1 = ax1.get_figure()
 ax1.set_ylim(0, max)
 
+# 두 번째 그래프의 크기를 설정, scode가 201부터 243까지의 데이터를 막대 그래프로 그림.
 plt.rcParams["figure.figsize"] = (39 * 3, 10)
 ax2 = implicit_metric.loc[201:243, :].plot.bar()
 fig2 = ax2.get_figure()
 ax1.set_ylim(0, max)
 
+# 세 번째 그래프의 크기를 설정, scode가 301부터 317까지의 데이터를 막대 그래프로 그림.
 plt.rcParams["figure.figsize"] = (39 * 3, 10)
 ax3 = implicit_metric.loc[301:317, :].plot.bar()
 fig3 = ax3.get_figure()
 ax1.set_ylim(0, max)
 
+# 네 번째 그래프의 크기를 설정, scode가 401부터 414까지의 데이터를 막대 그래프로 그림.
 plt.rcParams["figure.figsize"] = (39 * 3, 10)
 ax4 = implicit_metric.loc[401:414, :].plot.bar()
 fig4 = ax4.get_figure()
 ax1.set_ylim(0, max)
 
-# Save File
+# 그래프들을 SVG 파일로 저장. figs 리스트에 그래프 객체들을 저장, 각 그래프를 순차적으로 SVG 파일로 저장.
+# 인덱스가 0인 요소는 건너뛰고, 나머지 그래프는 line{idx}_implicit_metric_SUM.svg 형식의 파일명으로 저장.
 figs = [-1, fig1, fig2, fig3, fig4]
 for idx, f in enumerate(figs):
     if f == -1:
--- visualizationSession/implicit_metric_graph/implicit_metric_graph_subplot.py
+++ visualizationSession/implicit_metric_graph/implicit_metric_graph_subplot.py
@@ -3,50 +3,7 @@ import matplotlib.pyplot as plt
 
 df = pd.read_csv('sorted_implicit_metric1.csv', encoding='cp949')
 
-# def create_bar_plot(line_data, stations, station_numbers, line_name):
-#     plt.figure(figsize=(20, 6))
-#     plt.bar(stations, line_data.set_index('scode').reindex(stations)['implicit_metric'].fillna(0))
-#     plt.xlabel('Station Number')
-#     plt.ylabel('Implicit Metric')
-#     plt.title(f'Implicit Metric ({line_name})')
-#     plt.xticks(stations, station_numbers, rotation=45)
-#     plt.tight_layout()
-
-#     # 막대 그래프 상단에 implicit_metric 표시
-#     for i, v in enumerate(line_data.set_index('scode').reindex(stations)['implicit_metric'].fillna(0)):
-#         plt.text(i + stations[0] - 0.5, v + 0.01, str(round(v, 2)), ha='center', va='bottom')
-
-# line1 = df[df['LineCode'] == 1]
-# line2 = df[df['LineCode'] == 2]
-# line3 = df[df['LineCode'] == 3]
-# line4 = df[df['LineCode'] == 4]
-
-# stations_1 = list(range(95, 135))
-# stations_2 = list(range(201, 243))
-# stations_3 = list(range(301, 317))
-# stations_4 = list(range(401, 414))
-
-# station_numbers_1 = [str(scode) for scode in stations_1]
-# station_numbers_2 = [str(scode) for scode in stations_2]
-# station_numbers_3 = [str(scode) for scode in stations_3]
-# station_numbers_4 = [str(scode) for scode in stations_4]
-
-# # 1호선
-# create_bar_plot(line1, stations_1, station_numbers_1, "Line 1")
-# plt.show()
-
-# # 2호선
-# create_bar_plot(line2, stations_2, station_numbers_2, "Line 2")
-# plt.show()
-
-# # 3호선
-# create_bar_plot(line3, stations_3, station_numbers_3, "Line 3")
-# plt.show()
-
-# # 4호선
-# create_bar_plot(line4, stations_4, station_numbers_4, "Line 4")
-# plt.show()
-
+# 막대그래프를 생성하는 함수 create_bar_plot을 정의.
 def create_bar_plot(ax, line_data, stations, station_numbers, line_name):
     ax.bar(stations, line_data.set_index('scode').reindex(stations)['implicit_metric'].fillna(0))
     ax.set_xlabel('Station Number')
@@ -58,16 +15,19 @@ def create_bar_plot(ax, line_data, stations, station_numbers, line_name):
     # for i, v in enumerate(line_data.set_index('scode').reindex(stations)['implicit_metric'].fillna(0)):
     #     ax.text(i + stations[0] - 0.5, v + 0.01, str(round(v, 2)), ha='center', va='bottom')
 
+# 각 라인에 해당하는 데이터 필터링
 line1 = df[df['LineCode'] == 1]
 line2 = df[df['LineCode'] == 2]
 line3 = df[df['LineCode'] == 3]
 line4 = df[df['LineCode'] == 4]
 
+# 각 라인의 역 번호 범위 설정
 stations_1 = list(range(95, 135))
 stations_2 = list(range(201, 243))
 stations_3 = list(range(301, 317))
 stations_4 = list(range(401, 414))
 
+# 역 번호를 문자열로 변환하여 목록을 만듦.
 station_numbers_1 = [str(scode) for scode in stations_1]
 station_numbers_2 = [str(scode) for scode in stations_2]
 station_numbers_3 = [str(scode) for scode in stations_3]
